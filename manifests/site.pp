6070a66ec4dc2f863d7b9bf49635388ec63f7789
Christian Awelakoue <lacherv@users.noreply.github.com>
2020-10-15 04:09:57 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,6 @@
 node default { 
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner   => 'root',
-  }
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
 }
